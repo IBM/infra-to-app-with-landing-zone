885241205cb483e5c31c062b6ed4f0c3c65430af
Adam Geiger <ageiger@us.ibm.com>
2023-08-02 10:56:52 -0400

    Override template and diagram

--- main.tf
+++ main.tf
@@ -2,13 +2,15 @@
 # Landing Zone VSI Pattern
 ##############################################################################
 
+
+
 module "landing_zone" {
   source           = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone.git//patterns/vsi?ref=v4.4.4"
   prefix           = var.prefix
   region           = var.region
   ibmcloud_api_key = var.ibmcloud_api_key
   ssh_public_key   = var.ssh_key
-  override         = true # This line instructs the module to use the topology located in the file override.json
+  override_json_string = templatefile("./override.tftpl", { prefix = var.prefix})
 }
 
 module "app-install" {
